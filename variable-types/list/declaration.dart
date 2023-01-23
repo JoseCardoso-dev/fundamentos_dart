301768af9739d03f888513acf536d285e1f5e570
JrSouzaCardoso <jrcardoso541@gmail.com>
2023-01-23 18:28:13 -0300

    added new type: list

---
+++ variable-types/list/declaration.dart
@@ -0,0 +1,22 @@
+void main() {
+  //                0   1   2   3
+  List<int> list = [10, 15, 16, 20];
+  //List list; tipo dinamico
+  print(list[0]);
+
+  //List<int> list = [10, "abc", {"key": "valor"}];
+
+  List<String> listb;
+  listb = ["A", "B", "C"];
+  print(listb);
+
+  //instanciar lista vazia:
+  // List<String> listb; ERRADO
+  var listC = []; //CERTO
+
+  //vazia com tipo
+  var listD = <Map>[];
+
+  var listE = new List.empty();
+  var listF = List.empty();
+}
