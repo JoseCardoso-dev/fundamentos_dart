1701d7aa17658b8a6ce8c73345a35f61ca9a0a24
JrSouzaCardoso <jrcardoso541@gmail.com>
2023-01-23 22:58:40 -0300

    added new type: stream, and completed types data

---
+++ variable-types/stream/stream.dart
@@ -0,0 +1,33 @@
+//Stream = fluxo de dados
+
+import 'dart:async';
+
+void main() async {
+  final value = await futureFunction();
+  print(value);
+  streamFunction().listen((event) {
+    print("LETTER: ${event}");
+  });
+
+  final valueStream = Stream.value("TESTE");
+  valueStream.listen((event) {
+    print(event);
+  });
+}
+
+Stream<String> streamFunction() async* {
+  final word = "GABUL";
+  final list = word.split("");
+  for (var item in list) {
+    yield item;
+  }
+}
+
+Future<String> futureFunction() async {
+  final word = "GABUL";
+  final list = word.split("");
+  for (var item in list) {
+    return item;
+  }
+  return word;
+}
